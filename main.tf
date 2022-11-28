de67b8a77fcd23aea2ced322ff603035fe72c764
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-11-28 20:26:55 +0530

    updating ./terraform/roboshop-terraform-mutable

--- main.tf
+++ main.tf
@@ -6,9 +6,6 @@ module "VPC" {
   PUBLIC_SUBNET_CIDR   = var.PUBLIC_SUBNET_CIDR
   PRIVATE_SUBNET_CIDR  = var.PRIVATE_SUBNET_CIDR
   AZ                    = var.AZ
-  DEFAULT_VPC_ID        = var.DEFAULT_VPC_ID
-  DEFAULT_VPC_CIDR      = var.DEFAULT_VPC_CIDR
-  DEFAULT_VPC_RT        = var.DEFAULT_VPC_RT
 }
 
 
