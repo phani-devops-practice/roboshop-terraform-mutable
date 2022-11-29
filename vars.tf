599338ea2d3fed2ae23bfa89d52a415d5413d84f
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-11-29 16:05:30 +0530

    updating ./terraform/roboshop-terraform-mutable

--- vars.tf
+++ vars.tf
@@ -1,3 +1,4 @@
+// VPC
 variable "ENV" {}
 variable "PROJECT" {}
 variable "VPC_CIDR" {}
@@ -7,3 +8,14 @@ variable "AZ" {}
 variable "DEFAULT_VPC_ID" {}
 variable "DEFAULT_VPC_CIDR" {}
 variable "DEFAULT_VPC_RT" {}
+
+// RDS
+variable "RDS_ENGINE" {}
+variable "RDS_ENGINE_VERSION" {}
+variable "RDS_INSTANCE_CLASS" {}
+variable "RDS_PG_FAMILY" {}
+variable "PRIVATE_SUBNET_IDS" {}
+variable "RDS_PORT" {}
+variable "ALLOW_SG_CIDR" {}
+variable "VPC_ID" {}
+
