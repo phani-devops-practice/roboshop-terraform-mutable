4c23b3cac043d3f4fa9dfd77d67808f6ab2eaace
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-11-30 08:30:15 +0530

    updating ./terraform/roboshop-terraform-mutable

---
+++ main.tf
@@ -0,0 +1,12 @@
+module "VPC" {
+  source                = "github.com/phani-devops-practice/tf-module-vpc"
+  ENV                   = var.ENV
+  PROJECT               = var.PROJECT
+  VPC_CIDR              = var.VPC_CIDR
+  PUBLIC_SUBNETS_CIDR   = var.PUBLIC_SUBNETS_CIDR
+  PRIVATE_SUBNETS_CIDR  = var.PRIVATE_SUBNETS_CIDR
+  AZ                    = var.AZ
+  DEFAULT_VPC_ID        = var.DEFAULT_VPC_ID
+  DEFAULT_VPC_CIDR      = var.DEFAULT_VPC_CIDR
+  DEFAULT_VPC_RT        = var.DEFAULT_VPC_RT
+}
