7b411e75b9465e35112b93bc8639973ad24d9f76
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-12-22 07:10:02 +0530

    updating ./terraform/roboshop-terraform-mutable

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "REDIS_ENDPOINT" {
+  value = module.ELASTICACHE.REDIS_ENDPOINT
+}
+
+output "DOCDB_ENDPOINT" {
+  value = module.DOCDB.DOCDB_ENDPOINT
+}
+
+output "MYSQL_ENDPOINT" {
+  value = module.RDS.MYSQL_ENDPOINT
+}
